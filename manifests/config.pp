89150bb8a1453466c58dd22aec28fb5a7bdc2007
Carsten Thiel <thiel@sub.uni-goettingen.de>
2016-02-23 13:48:23 +0100

    change defaults

--- manifests/config.pp
+++ manifests/config.pp
@@ -27,7 +27,7 @@ class dariahshibboleth::config (
   $discoveryurl                   = undef,
   $handlerurl_prefix              = undef,
   $remote_user_pref_list          = undef,
-  $attribute_checker_flushsession = undef,
+  $attribute_checker_flushsession = true,
   $disable_scoping_check          = false,
   $handlerssl                     = true,
 ) inherits dariahshibboleth::params {
