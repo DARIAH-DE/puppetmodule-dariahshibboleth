349fb13b93acbcd20aab6d8b59d18630839e603c
Carsten Thiel <thiel@sub.uni-goettingen.de>
2018-01-03 12:31:16 +0100

    switch to pdk

--- spec/acceptance/shib_install_spec.rb
+++ spec/acceptance/shib_install_spec.rb
@@ -2,13 +2,12 @@ require 'spec_helper_acceptance'
 
 describe 'dariahshibboleth class' do
   it 'applies the module' do
-    apply_manifest(%{
+    apply_manifest(%(
       class { 'dariahshibboleth': }
-    }, :catch_failures => true)
+    ), catch_failures: true)
   end
 
   describe package('shibboleth') do
-    it { should be_installed }
+    it { is_expected.to be_installed }
   end
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,23 @@
-require 'rspec-puppet'
+require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+default_facts = {
+  puppetversion: Puppet.version,
+  facterversion: Facter.version,
+}
 
-RSpec.configure do |c|
-  c.module_path = File.join(fixture_path, 'modules')
-  c.manifest_dir = File.join(fixture_path, 'manifests')
-  c.environmentpath = File.join(Dir.pwd, 'spec')
+default_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml'))
+default_module_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml'))
+
+if File.exist?(default_facts_path) && File.readable?(default_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_facts_path)))
 end
 
-require 'coveralls'
-Coveralls.wear!
+if File.exist?(default_module_facts_path) && File.readable?(default_module_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_module_facts_path)))
+end
 
+RSpec.configure do |c|
+  c.default_facts = default_facts
+end
