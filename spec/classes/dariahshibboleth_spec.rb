5a8206c355ec08e7d54cd9e8973bbea43b7bfe09
Carsten Thiel <thiel@sub.uni-goettingen.de>
2018-04-03 17:55:55 +0200

    Ruby style

--- spec/classes/dariahshibboleth_spec.rb
+++ spec/classes/dariahshibboleth_spec.rb
@@ -170,7 +170,6 @@ describe 'dariahshibboleth' do
           is_expected.to contain_file('/etc/shibboleth/shibboleth2.xml') \
             .with_content(%r{<MetadataProvider type="XML" validate="true" file="dariah-proxy-idp.xml"\/>}) \
             .with_content(%r{<SSO entityID="https:\/\/aaiproxy.de.dariah.eu\/idp">})
-
         end
       end
 
